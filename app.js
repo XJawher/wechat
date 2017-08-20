06d2a3a1a8cbdd14434f0d9291c179e1c1b7aaff
xiancai <lplee7830@gmail.com>
2017-08-20 21:34:57 +0800

    修改 app.js 的generator 函数

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 'use strict' 
 let koa = require('koa')
-let sha1 = require('sha1')  
+let sha1 = require('sha1')
+let g = require('./wechat/g')
 let config = {
   wechat : {
-    appID :  '你的 appID',
-    appSecret : '你的 appSecret',
-    token : '你的 token' 
+    appID :  'wxb529535807ed1405',
+    appSecret : '99a54169f924330cbe5d3db83739eaf5',
+    token : 'lipc_token' 
   }
 }  
 let app = new koa()
-app.use(function *(next) {
-  console.log(this.query)
-//实现验证逻辑   
-  let token = config.wechat.token
-  let signature = this.query.signature
-  let nonce = this.query.nonce
-  let timestamp = this.query.timestamp
-  let echostr = this.query.echostr
-  let str = [token,timestamp,nonce].sort().join('')
-  let sha = sha1(str)
-
-      if (sha === signature) {
-        this.body = echostr + ''
-      }
-      else {
-        this.body = 'wrong'
-      }
-
-})
+app.use(wechat(config.wechat))
 app.listen(8087)
 console.log('listening 8087 ....')
